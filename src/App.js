db017ecbecfadbc0380b04598d2d7679531c92b6
MartaMX <92443035+MartaMX@users.noreply.github.com>
2021-12-02 20:29:18 +0100

    REACT "HTML" structure and temperature response

--- src/App.js
+++ src/App.js
@@ -7,23 +7,67 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="wrapper">
-          <h1>Weather React</h1>
           <p>Search</p>
+          <div className="search">
+            <form id="search-form">
+              <div className="row search">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Type a city.."
+                    class="form-control"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
 
           <p>DemoCity</p>
-          <p>
-            Democonditions
-            <ul>
-              <li>
-                <WeatherInfo />
-              </li>
-              <li>
-                <span>DemoDate</span>, DemoDescription
-              </li>
-              <li>DemoHumidity, Demowind</li>
-            </ul>
-          </p>
-          <p>25ª</p>
+          <div className="row">
+            <div className="col-7">
+              <h1 id="city">New York</h1>
+              <ul>
+                <li>
+                  <span id="day"> Friday</span>
+                  <span id="time"> 5:00,</span>
+                  <span id="description"> Clear</span>
+                </li>
+                <li>
+                  Humidity: <span id="humidity">56</span>%, Wind:
+                  <span id="wind">0.89</span>
+                  km/h
+                </li>
+              </ul>
+            </div>
+            <div className="col-5 right">
+              <div className="clearfix weather-temperature">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/414/414927.png"
+                  width="50"
+                  class="float-left"
+                  id="today-icon"
+                />
+                <span className="float-left temperature" id="temperature">
+                  25
+                  <WeatherInfo />
+                </span>
+                <span className="float-left unit">
+                  <a href="#" class="active" id="celsius">
+                    °C
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function WeatherInfo() {
+  let [temperat, setTemperat] = useState(0);
   function getTemperature(response) {
-    //console.log(response);
-    let temperat = Math.round(response.data.main.temp);
-    alert(temperat);
+    console.log(response);
+    console.log(response.data.main.temp);
+    setTemperat(Math.round(response.data.main.temp));
+    //alert(temperat);
   }
   let url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=f020078c4a89cd61e6aa94f2028cbd7e&units=metric`;
   axios.get(url).then(getTemperature);
-  return "Aquí va la temperatura";
+  return <span> {temperat} </span>;
 }
