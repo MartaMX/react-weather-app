11b79a01c0c16c41af7246b3b05b8d9326e941fc
MartaMX <92443035+MartaMX@users.noreply.github.com>
2021-12-15 15:02:47 +0100

    REACT JS Connect search form and API

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import React, { useState } from "react";
 import axios from "axios";
-import WeatherInfo from "./WeatherInfo";
 
 function App() {
-  const [weatherDate, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState("New York");
   function handleResponse(response) {
     setWeatherData({
-      temperature: response.data.main.temp,
+      temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
     });
   }
   function Search() {
-    //const apiKey = "020078c4a89cd61e6aa94f2028cbd7";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=f020078c4a89cd61e6aa94f2028cbd7e&units=metric`;
+    const apiKey = "f020078c4a89cd61e6aa94f2028cbd7e";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(handleResponse);
   }
   function handleSubmit(event) {
@@ -29,7 +30,6 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="wrapper">
-          <p>Search</p>
           <div className="search">
             <form onSubmit={handleSubmit} id="search-form">
               <div className="row search">
@@ -55,7 +55,6 @@ function App() {
             </form>
           </div>
 
-          <p>DemoCity</p>
           <div className="row">
             <div className="col-7">
               <h1 id="city">{city}</h1>
@@ -63,11 +62,12 @@ function App() {
                 <li>
                   <span id="day"> Friday</span>
                   <span id="time"> 5:00,</span>
-                  <span id="description"> Clear</span>
+                  <span id="description"> {weatherData.description}</span>
                 </li>
                 <li>
-                  Humidity: <span id="humidity">56</span>%, Wind:
-                  <span id="wind">0.89</span>
+                  Humidity: <span id="humidity"> {weatherData.humidity} </span>
+                  %, Wind:
+                  <span id="wind">{weatherData.wind}</span>
                   km/h
                 </li>
               </ul>
@@ -81,8 +81,7 @@ function App() {
                   id="today-icon"
                 />
                 <span className="float-left temperature" id="temperature">
-                  25
-                  <WeatherInfo />
+                  {weatherData.temperature}
                 </span>
                 <span className="float-left unit">
                   <a href="#" class="active" id="celsius">
